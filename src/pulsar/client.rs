bb1e6fcb3ce2bc56206c0f52cbe5d36356fd4ba1
Ted Pilcher <tedspilcher@gmail.com>
2025-03-14 20:23:14 +0000

    Implement create_consumer method for PulsarClient

--- src/pulsar/client.rs
+++ src/pulsar/client.rs
@@ -1,9 +1,13 @@
-use pulsar::{producer, proto, Producer, Pulsar, TokioExecutor};
+use pulsar::{
+    consumer::DeadLetterPolicy, producer, proto, Consumer, DeserializeMessage, Producer, Pulsar,
+    SubType, TokioExecutor,
+};
 
 const PULSAR_ADDR: &str = "pulsar://localhost:6650";
+const MAX_JOB_RETRY: usize = 3;
 
 pub struct PulsarClient {
-    internal_client: Pulsar<TokioExecutor>,
+    pub internal_client: Pulsar<TokioExecutor>,
 }
 
 impl PulsarClient {
@@ -31,5 +35,28 @@ impl PulsarClient {
             .build()
             .await
             .expect("Should be able to create producer")
+    }
+
+    pub async fn create_consumer<T>(
+        &self,
+        topic: &str,
+        sub_type: SubType,
+        subscription: &str,
+    ) -> Consumer<T, TokioExecutor>
+    where
+        T: DeserializeMessage,
+    {
+        self.internal_client
+            .consumer()
+            .with_topic(topic)
+            .with_subscription_type(sub_type) // exclusive for current testing
+            .with_subscription(subscription)
+            .with_dead_letter_policy(DeadLetterPolicy {
+                max_redeliver_count: MAX_JOB_RETRY,
+                dead_letter_topic: "DLQ".to_string(),
+            })
+            .build()
+            .await
+            .expect("Should be able to create consumer")
     }
 }
