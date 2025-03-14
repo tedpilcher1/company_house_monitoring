f2bcb8c46664d9e8550c630081f044098ab5bc95
Ted Pilcher <tedspilcher@gmail.com>
2025-03-14 18:31:36 +0000

    Add required database models

---
+++ src/database/models.rs
@@ -0,0 +1,39 @@
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable, Selectable};
+use serde_json::Value;
+use uuid::Uuid;
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::database::schema::company)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct ProcessedUpdate {
+    pub company_house_id: Uuid,
+    pub first_monitored_at: NaiveDateTime,
+}
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::database::schema::subscription)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Subscription {
+    pub id: Uuid,
+    pub company_house_id: String,
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::database::schema::notable_change)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NotableChange {
+    pub id: Uuid,
+    pub subscription_id: Uuid,
+    pub field: String,
+}
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::database::schema::company_snapshot)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct CompanySnapshot {
+    pub id: Uuid,
+    pub company_house_id: String,
+    pub snapshot_data: Value,
+}
