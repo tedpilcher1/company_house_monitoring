2ef7a4877eec946ae46790663be68f29a5edbb3e
Ted Pilcher <tedspilcher@gmail.com>
2025-03-14 22:03:42 +0000

    Add /unsubscribe{subscription_id} endpoint

--- src/endpoints/api_endpoints.rs
+++ src/endpoints/api_endpoints.rs
@@ -1,4 +1,5 @@
-use actix_web::{put, web, HttpResponse, Responder};
+use actix_web::{delete, put, web, HttpResponse, Responder};
+use uuid::Uuid;
 
 use crate::endpoints::{AppState, SubscribeRequest};
 
@@ -10,7 +11,7 @@ async fn subscribe_endpoint(
 ) -> impl Responder {
     let subscribe_request = request_body.into_inner();
     if let Ok(mut database) = state.database.lock() {
-        let res = match database.create_subscription(
+        return match database.create_subscription(
             company_house_id.to_string(),
             subscribe_request.notable_changes,
             subscribe_request.url,
@@ -18,7 +19,20 @@ async fn subscribe_endpoint(
             Ok(subscription_id) => HttpResponse::Ok().json(subscription_id),
             Err(_) => HttpResponse::InternalServerError().finish(),
         };
-        return res;
+    }
+    HttpResponse::InternalServerError().finish()
+}
+
+#[delete("/unsubscribe/{subscription_id}")]
+async fn unsubscribe_endpoint(
+    subscription_id: web::Path<Uuid>,
+    state: web::Data<AppState>,
+) -> impl Responder {
+    if let Ok(mut database) = state.database.lock() {
+        return match database.delete_subscription(*subscription_id) {
+            Ok(_) => HttpResponse::Ok().finish(),
+            Err(_) => HttpResponse::NotFound().finish(),
+        };
     }
     HttpResponse::InternalServerError().finish()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::sync::{Arc, Mutex};
 use actix_web::{App, HttpServer};
 use company_house_monitoring::{
     database::client::DatabaseClient,
-    endpoints::{api_endpoints::subscribe_endpoint, AppState},
+    endpoints::{
+        api_endpoints::{subscribe_endpoint, unsubscribe_endpoint},
+        AppState,
+    },
 };
 
 #[actix_web::main]
@@ -18,6 +21,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(app_state.clone())
             .service(subscribe_endpoint)
+            .service(unsubscribe_endpoint)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
