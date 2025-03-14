30e9deaa56713bc39a5688c865e53fdbbd6d6fa8
Ted Pilcher <tedspilcher@gmail.com>
2025-03-14 18:31:47 +0000

    Add initial db migration

---
+++ migrations/2025-03-14-183107_add_inital_tables/up.sql
@@ -0,0 +1,24 @@
+-- Your SQL goes here
+CREATE TABLE "company"(
+	"company_house_id" UUID NOT NULL PRIMARY KEY,
+	"first_monitored_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "company_snapshot"(
+	"id" UUID NOT NULL PRIMARY KEY,
+	"company_house_id" TEXT NOT NULL,
+	"snapshot_data" JSONB NOT NULL
+);
+
+CREATE TABLE "subscription"(
+	"id" UUID NOT NULL PRIMARY KEY,
+	"company_house_id" TEXT NOT NULL,
+	"created_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "notable_change"(
+	"id" UUID NOT NULL PRIMARY KEY,
+	"subscription_id" UUID NOT NULL,
+	"field" TEXT NOT NULL
+);
+
